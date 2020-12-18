540f53ce4b84c0bf01a38d64557218bdace48449
Louis Rex <rlhardrock@gmail.com>
2020-12-18 02:34:52 -0500

    boton log out

--- src/components/auth/home.vue
+++ src/components/auth/home.vue
@@ -1,35 +1,21 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #3DA6A0;">
+    <!-- <nav class="navbar navbar-expand-lg navbar-dark" ;"> -->
+    <nav class="navbar navbar-light bg-dark" style="background-color: #3DA6A0">
+            <div class="container-fluid">
+         
+                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                    <button class="btn btn-dark" type="button"> </button>
+                </div>
+                <form class="d-flex">
+                <button 
+                @click.prevent="logout"
+                class="btn btn-warning btn-lg"
+                type="buttom">Log Out</button>
 
-      <div class="container">
-        <a class="navbar-brand" href="#"> 
-          
-          <img src="https://www.flaticon.es/svg/static/icons/svg/1177/1177568.svg" width="30 " height="30 " class="d-inline-block align-top " alt=" " loading="lazy "> 
-          Usuario</a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div
-          class="collapse navbar-collapse justify-content-end"
-          id="navbarNav"
-        >
-          <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" @click="logUserOut"> Cerrar Sesión</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+                </form>
+            </div>
+        </nav>
     <section>
       <div class="container mt-5">
         <div class="row">
